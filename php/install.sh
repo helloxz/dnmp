1384a87d32d32d621aae341880c15d2090f0d4d9
xiaoz <xiaoz93@outlook.com>
2021-03-11 19:01:19 +0800

    一些优化

--- php/install.sh
+++ php/install.sh
@@ -1,48 +1,45 @@
 #!/bin/bash
-#update system
-apt-get update
-apt-get install -y wget
-#install nginx
+#更新软件
+apk update
+#安装timezone
+apk add -U tzdata
+#查看时区列表
+ls /usr/share/zoneinfo
+#拷贝需要的时区文件到localtime
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+#查看当前时间
+date
+#为了精简镜像，可以将tzdata删除了
+apk del tzdata
+#安装wget
+apk add wget
+
+
+#安装nginx
 function install_nginx() {
-	cd /usr/local
-	#add user and group
-	groupadd www
-	useradd -M -g www www -s /sbin/nologin
-	#download nginx
-	wget http://soft.xiaoz.org/nginx/xcdn-binary-1.18-debian.tar.gz
-	tar -zxvf xcdn-binary-1.18-debian.tar.gz
-	rm -rf xcdn-binary-1.18-debian.tar.gz
-	#add env
-	#echo "export PATH=$PATH:/usr/local/nginx/sbin" >> /etc/profile
-	#export PATH=$PATH:'/usr/local/nginx/sbin'
-	#备份配置文件
-	cd /usr/local/nginx/conf
-	cp nginx.conf nginx.conf.bak
-	wget -P /usr/local/nginx/conf https://github.com/helloxz/dnmp/raw/main/php/nginx.conf
+	#安装nginx
+	apk add nginx
+	wget -P /etc/nginx https://github.com/helloxz/dnmp/raw/main/php/nginx.conf
 }
-
-#setting php
-function set_php() {
+#设置PHP
+function set_php(){
+	#安装依赖
+	apk add autoconf gcc musl-dev g++ zlib-dev make
+	#安装GD库 依赖
+	#apk add libpng-dev
+	#设置php.ini
 	cp /usr/local/etc/php/php.ini-development /usr/local/etc/php/php.ini
-	#install gd ext
-	apt-get update && apt-get install -y \
-        libfreetype6-dev \
-        libjpeg62-turbo-dev \
-        libpng-dev \
-        libmagickwand-dev --no-install-recommends \
-    && docker-php-ext-configure gd --with-freetype --with-jpeg \
-    && docker-php-ext-install -j$(nproc) gd
-    #install xdebug and redis
-    pecl install redis-5.1.1 \
+	#zend_extension=/usr/local/lib/php/extensions/no-debug-non-zts-20180731/xdebug.so
+	#
+	sed -i "s/www-data/www/g" /usr/local/etc/php-fpm.d/www.conf
+	#安装扩展
+	pecl install redis-5.1.1 \
     && pecl install xdebug-2.8.1 \
-    && pecl install imagick \
-    && docker-php-ext-enable redis xdebug imagick
-    sed -i "s/www-data/www/g" /usr/local/etc/php-fpm.d/www.conf
-    rm -rf /var/lib/apt/lists/*
+    && docker-php-ext-enable redis xdebug
 }
 
-install_nginx && set_php
-chmod +x /usr/sbin/run.sh
+
+
 echo '-----------------------------'
 echo 'nginx + php install success.'
 echo '-----------------------------'
