fcd2b284c905b996f6473ccdc23973c9d2c848a0
xiaoz <xiaoz93@outlook.com>
2023-11-22 18:19:46 +0800

    fix bug

--- nginx/sh/run.sh
+++ nginx/sh/run.sh
@@ -5,32 +5,16 @@
 #Github:https://github.com/helloxz/xcdn
 ####################### END #######################
 
-#创建xcdn所需目录
-function create_dir(){
-    #创建配置文件夹
-    mkdir -p /data/xcdn/conf/vhost;
-    mkdir -p /data/xcdn/conf/cdn;
-    mkdir -p /data/xcdn/conf/stream;
-
-    #创建日志文件夹
-    mkdir -p /data/xcdn/logs;
-    touch /data/xcdn/logs/error.log
-    #创建ssl证书文件夹
-    mkdir -p /data/xcdn/ssl;
-    #创建缓存文件夹
-    mkdir -p /data/xcdn/caches;
-    chmod -R 777 /data/xcdn/caches;
-}
 
 #运行时检查
 function run_check(){
 	#检查nginx日志是否存在，如果不存在则创建
-	if [ ! -f "/data/xcdn/logs/error.log" ]
+	if [ ! -f "/usr/local/nginx/logs/error.log" ]
 	then
 		#创建日志文件夹
-	    mkdir -p /data/xcdn/logs;
-	    touch /data/xcdn/logs/error.log
-	    touch /data/xcdn/logs/stream-access.log
+	    # mkdir -p /data/xcdn/logs;
+	    touch /usr/local/nginx/logs/error.log
+	    touch /usr/local/nginx/logs/stream-access.log
 	fi
 
 }
